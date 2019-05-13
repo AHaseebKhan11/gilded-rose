972ce525164e24e42e1e639d68b2aee7cbdd89b3
Haseeb Khan <abdulhaseeb.khn@gmail.com>
2019-05-14 03:00:36 +0500

    Add test cases

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,129 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+  describe 'update_quality function' do
+
+    describe 'Concert items' do
+      it 'increases to 50 when sell_in above 10 and quality is 49' do
+        item = Item.new('Backstage passes to a TAFKAL80ETC concert', sell_in=15, quality=49)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq 14
+        expect(item.quality).to eq 50
+      end
+
+      it 'Quality stays same for back stage pass' do
+        item = Item.new('Backstage passes to a TAFKAL80ETC concert', sell_in=15, quality=50)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq 14
+        expect(item.quality).to eq 50
+      end
+
+      it 'Quality reduces to zero once concert date has passed' do
+        item = Item.new('Backstage passes to a TAFKAL80ETC concert', sell_in=0, quality=50)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq -1
+        expect(item.quality).to eq 0
+      end
+    end
+
+
+    describe 'Regular items' do
+      it 'Quality reduces twice as fast for regular items' do
+        item = Item.new('Lorem ispum', sell_in=0, quality=10)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq -1
+        expect(item.quality).to eq 8
+      end
+    end
+
+    describe 'Exceptions' do
+      it 'Raise exception when quality is negative' do
+        item = Item.new('Lorem ispum', sell_in=0, quality=-10)
+        items = [item]
+        expect { described_class.new(items) }.to raise_exception('Incorrect quality value supplied')
+      end
+
+      it 'Raise exception when quality is too high' do
+        item = Item.new('Lorem ispum', sell_in=0, quality=60)
+        items = [item]
+        expect { described_class.new(items) }.to raise_exception('Quality value too high')
+      end
+
+      it 'Raise exception when quality is incorrect for legendary item' do
+        item = Item.new('Sulfuras, Hand of Ragnaros', sell_in=1, quality=60)
+        items = [item]
+        expect { described_class.new(items) }.to raise_exception('Give a positive quality value of 80 for legendary items')
+      end
+    end
+
+    describe 'Conjured items delay twice as fast' do
+      it 'Sell in date has not passed' do
+        item = Item.new('Conjured', sell_in=2, quality=10)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq 1
+        expect(item.quality).to eq 8
+      end
+
+      it 'Sell in date has passed and quality is not less than zero' do
+        item = Item.new('Conjured', sell_in=-1, quality=3)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq -2
+        expect(item.quality).to eq 0
+      end
+    end
+
+    describe 'Sulfuras items' do
+      it 'Sell in date has not passed' do
+        item = Item.new('Sulfuras, Hand of Ragnaros', sell_in=2, quality=80)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        # Sell in date or qulity is not affected for Sulfurus
+        expect(item.sell_in).to eq 2
+        expect(item.quality).to eq 80
+      end
+
+      it 'Sell in date has passed and quality is not less than zero' do
+        item = Item.new('Sulfuras, Hand of Ragnaros', sell_in=-1, quality=80)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        # Sell in date or qulity is not affected for Sulfurus
+        expect(item.sell_in).to eq -1
+        expect(item.quality).to eq 80
+      end
+    end
+
+    describe 'Aged brie' do
+      it 'Sell in date has not passed' do
+        item = Item.new('Aged Brie', sell_in=2, quality=10)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq 1
+        expect(item.quality).to eq 11
+      end
+
+      it 'Aged brie increases twice as fast when sell in date has passed' do
+        item = Item.new('Aged Brie', sell_in=-1, quality=3)
+        items = [item]
+        gilded_rose = described_class.new(items)
+        gilded_rose.update_quality
+        expect(item.sell_in).to eq -2
+        expect(item.quality).to eq 5
+      end
     end
   end
 end
--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -1,10 +1,43 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'test/unit'
+require File.join(File.dirname(__FILE__), 'texttest_fixture')
 
 class TestUntitled < Test::Unit::TestCase
-  def test_foo
-    items = [Item.new('foo', 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal items[0].name, 'fixme'
+  attr_accessor :fixtures
+
+  def setup
+    @fixtures = TextTestFixture.new
   end
+
+  def general_item_values
+    item = fixtures.gilded_rose.items[0]
+    fixtures.gilded_rose.update_quality
+    assert_equal 19, item.quality
+    assert_equal 9, item.sell_in
+  end
+
+  def test_sulfuras_doesnt_age
+    item = Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=1, quality=80)
+    g = GildedRose.new([item]).update_quality
+    assert_equal 80, item.quality
+    assert_equal 1, item.sell_in
+  end
+
+  def legend_item_value_check
+    assert_raise { GildedRose.new([Item.new(name="Sulfuras, Hand of Ragnaros",
+                                            sell_in=0, quality=1000)]) }
+  end
+
+  def raise_error_quality_negative
+    assert_raise { GildedRose.new([Item.new(name="Abc", sell_in=0, quality=-1)]) }
+  end
+
+  def test_update_quality_conjured
+    item = Item.new(name="Conjured Mana Cake", sell_in=10, quality=50)
+    g = GildedRose.new([item])
+    g.update_quality
+    assert_equal item.quality, 48
+    assert_equal item.sell_in, 9
+  end
+
 end
