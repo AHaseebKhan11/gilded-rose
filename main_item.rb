dad1f400d8b26dba9b559a1f9f4b72ba1f332679
Haseeb Khan <abdulhaseeb.khn@gmail.com>
2019-05-15 14:44:44 +0500

    Remove redundant constant objects

--- main_item.rb
+++ main_item.rb
@@ -1,7 +1,6 @@
 class MainItem
   Dir[File.dirname(__FILE__) + '/item_classes/*.rb'].each {|file| require file }
   attr_reader :item
-  SULFURUS = 'Sulfuras, Hand of Ragnaros'
   ITEM_HASH = {
                 'Aged Brie' => AgedBrie,
                 'Sulfuras, Hand of Ragnaros' => Sulf,
@@ -11,19 +10,23 @@ class MainItem
 
   def initialize(item)
     @item = item
-    raise 'Quality value too high' if item.quality > 50 && item.name != SULFURUS
-    raise 'Incorrect quality value supplied' if item.quality < 0
+    raise_exceptions
   end
 
   def update
-    if item.quality > 0
-      item.quality -= 1
-      item.quality -= 1 if item.sell_in < 1
-    end
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 1
+    item.quality = 0 if item.quality < 0
     item.sell_in -= 1
   end
 
   def self.build_items(i)
     ITEM_HASH[i.name].nil? ? MainItem.new(i) : ITEM_HASH[i.name].new(i)
+  end
+
+  private
+  def raise_exceptions
+    raise 'Quality value too high' if item.quality > 50 && ITEM_HASH[item.name] != Sulf
+    raise 'Incorrect quality value supplied' if item.quality < 0
   end
 end
